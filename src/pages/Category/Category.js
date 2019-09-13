7dc75f3ae552e76d72d51b8c3c3fb1666d273951
resita-alterra <resita@alterra.id>
2019-09-13 21:07:31 +0700

    sukses get trash categories

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -20,7 +20,8 @@ class Category extends Component {
         this.name = React.createRef();
         this.doSubmit = this.doSubmit.bind(this);
         this.state = {
-            activeItem: "1"
+            activeItem: "1",
+            categories: []
         }
     }
 
@@ -34,8 +35,7 @@ class Category extends Component {
     };
 
     doSubmit = e => {
-        console.log('Masuk')
-        console.log(this.name)
+
         e.preventDefault();
         const self = this;
         let config = {
@@ -50,6 +50,24 @@ class Category extends Component {
         };
         axios(config).then(function (response) {
             console.log(response)
+        }).catch(function (error) {
+            console.log(error)
+        })
+    }
+
+    componentDidMount = () => {
+        const self = this;
+
+        let config = {
+            method: "GET",
+            url: self.props.url + "/v1/trash_category",
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("admin_token")
+            }
+        }
+        axios(config).then(function (response) {
+            self.setState({ categories: response.data })
+
         }).catch(function (error) {
             console.log(error)
         })
@@ -113,19 +131,23 @@ class Category extends Component {
                                         <tbody>
 
 
-                                            <tr>
-                                                <td valign="bottom"> 1</td>
-                                                <td valign="bottom"> Plastik</td>
-                                                <td valign="bottom"> tanggalsekarang</td>
-                                                <td valign="bottom"> tanggalsekarang</td>
-                                                <td valign="bottom"> <button className="btn btn-lg btn-primary btn-block rounded-pill" type="submit" style={{ padding: "4px" }} valign="center" >
-                                                    Edit
+                                            {this.state.categories.map((elm, key) => {
+                                                return (
+                                                    <tr>
+                                                        <td valign="bottom"> {elm.id}</td>
+                                                        <td valign="bottom"> {elm.category_name}</td>
+                                                        <td valign="bottom"> {elm.created_at}</td>
+                                                        <td valign="bottom"> {elm.updated_at}</td>
+                                                        <td valign="bottom"> <button className="btn btn-lg btn-primary btn-block rounded-pill" type="submit" style={{ padding: "4px" }} valign="center" >
+                                                            Edit
                                                         </button>
-                                                </td>
-                                                <td valign="bottom"> <button className="btn btn-lg btn-danger btn-block rounded-pill" type="submit" style={{ padding: "4px" }}>
-                                                    Hapus
+                                                        </td>
+                                                        <td valign="bottom"> <button className="btn btn-lg btn-danger btn-block rounded-pill" type="submit" style={{ padding: "4px" }}>
+                                                            Hapus
                                                 </button></td>
-                                            </tr>
+                                                    </tr>
+                                                )
+                                            })}
 
 
 
