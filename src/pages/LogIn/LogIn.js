b8e56fa04f12a59d8a7d8fc37f137bb6f859b903
Aulia <ahanifan@alterra.id>
2019-09-13 18:35:49 +0700

    logic for admin fixed

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
     MDBBtn,
     MDBCol,
     MDBContainer,
     MDBRow,
     MDBInput
-} from "mdbreact";
-import axios from "axios";
-import { connect } from "unistore/react";
-import { actions } from "../../store";
-import { Redirect, Link } from 'react-router-dom'
+} from 'mdbreact';
+import axios from 'axios';
+import { connect } from 'unistore/react';
+import { actions } from '../../store';
 import './LogIn.css'
+import Swal from 'sweetalert2'
 
 class LogIn extends Component {
     constructor(props) {
@@ -25,56 +25,90 @@ class LogIn extends Component {
         e.preventDefault();
         const self = this;
         await axios
-            .post(this.props.url + "/v1/auth", {
+            .post(this.props.url + '/v1/auth', {
                 email: this.email.current.value,
                 password: this.password.current.value
             })
             .then(response => {
-                localStorage.setItem('admin_logged_in', true)
                 localStorage.setItem('admin_token', response.data.token)
-                this.props.history.push('/')
+                // get action to check the role if is it ADMIN or NOT
+                axios
+                    .get(this.props.url + '/v1/auth', {
+                        headers: {
+                            Authorization: "Bearer " + response.data.token
+                        }
+                    })
+                    .then(response => {
+                        console.log(response.data.claims.role)
+                        if (response.data.claims.role === true) {
+                            localStorage.setItem('admin_logged_in', true)
+                            this.props.history.push('/')
+                            Swal.fire({
+                                type: 'success',
+                                title: 'Success',
+                                text: 'Anda Berhasil Login!'
+                            })
+                        } else {
+                            Swal.fire({
+                                type: 'error',
+                                title: 'Oops...',
+                                text: 'Password atau Email anda Salah!'
+                            })
+                        }
+                    })
+                    .catch(error => {
+                        Swal.fire({
+                            type: 'error',
+                            title: 'Oops...',
+                            text: 'Password atau Email anda Salah!'
+                        })
+                    });
             })
             .catch(error => {
-                alert('Password atau Email Salah!')
+                Swal.fire({
+                    type: 'error',
+                    title: 'Oops...',
+                    text: 'Password atau Email anda Salah!'
+                })
             });
     };
 
     render() {
         return (
-            <div style={{ height: "100vh" }} id="login">
+            <div style={{ height: '100vh' }} id='login'>
                 <MDBContainer >
-                    <MDBRow center='true' className="justify-content-center">
-                        <MDBCol md="6" >
+                    <MDBRow center='true' className='justify-content-center'>
+                        <MDBCol md='6' >
                             <br />
                             <br />
                             <br />
-                            <h1 className="text-center">Dasbor Admin</h1>
+                            <h1 className='text-center'>Dasbor Admin</h1>
                             <br />
                             <form>
-                                <p className="h4 text-center mb-4">Login</p>
-                                <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                                <p className='h4 text-center mb-4'>Login</p>
+                                <label htmlFor='defaultFormLoginEmailEx' className='grey-text'>
                                     Email:
                                 </label>
                                 <input
-                                    type="email"
-                                    id="defaultFormLoginEmailEx"
-                                    className="form-control"
+                                    type='email'
+                                    id='defaultFormLoginEmailEx'
+                                    className='form-control'
                                     ref={this.email}
                                     required
                                 />
                                 <br />
-                                <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+                                <label htmlFor='defaultFormLoginPasswordEx' className='grey-text'>
                                     Password:
                                 </label>
                                 <input
-                                    type="password"
-                                    id="defaultFormLoginPasswordEx"
-                                    className="form-control"
+                                    type='password'
+                                    id='defaultFormLoginPasswordEx'
+                                    className='form-control'
                                     ref={this.password}
                                     required
                                 />
-                                <div className="text-center mt-4">
-                                    <MDBBtn className="rounded-pill" style={{ width: "145px", fontWeight: "700" }} onClick={this.doLogIn} id="LoginButton" type="submit">Masuk</MDBBtn>
+                                <div className='text-center mt-4'>
+                                    <MDBBtn className='rounded-pill' style={{ width: '145px', fontWeight: '700' }} onClick={this.doLogIn} id='LoginButton' type='submit'>Masuk</MDBBtn>
                                 </div>
                             </form>
                         </MDBCol>
@@ -85,4 +119,4 @@ class LogIn extends Component {
     }
 }
 
-export default connect("url", actions)(LogIn);
+export default connect('url', actions)(LogIn);
