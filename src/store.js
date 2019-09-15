5ee8620c1b9e8dccf4a569e3d5a01f0d62b72e0e
resita-alterra <resita@alterra.id>
2019-09-15 16:45:34 +0700

    mengembalikan url

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import createStore from 'unistore';
 
 export const store = createStore({
-  // url: 'http://backend.fikriamri.xyz',
-  url: 'http://localhost:5000',
+  url: 'http://backend.fikriamri.xyz',
+  // url: 'http://localhost:5000',
 });
 
 export const actions = (store) => ({
